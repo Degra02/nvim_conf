783f571c32d3faf8f2fe60c49ec7ec7fd93d6e47
Degra02 <filippodegrandi02@gmail.com>
2024-08-22 23:14:05 +0200

    changes

--- lua/degra/plugins/copilot.lua
+++ lua/degra/plugins/copilot.lua
@@ -1,3 +1,33 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = false,
+        -- auto_trigger = true,
+        debounce = 100,
+        keymap = {
+          accept = "<C-l>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        rust = true,
+        zig = true,
+        c = true,
+        cpp = true,
+        tex = true,
+        css = true,
+        go = true,
+        html = true,
+        javascript = true,
+        lua = true,
+        typescript = true,
+        ["*"] = false,
+      },
+      server_opts_overrides = {},
+    })
+  end,
 }
